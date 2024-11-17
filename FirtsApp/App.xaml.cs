0c93c11a92efc319059d16622f67231216e51485
PixelFXS <vjcarlosid@gmail.com>
2024-11-16 19:14:17 -0600

    Agregar archivos de proyecto.

---
+++ FirtsApp/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace FirtsApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
